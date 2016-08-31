a407011cc13ec87bcac4bc7ccb75e2bb12691809
Adam Dinwoodie <adam@dinwoodie.org>
2016-08-31 16:04:14 +0100

    Convert Species class to a namedtuple
    The `Species` class isn't doing anything clever, so just treat it as a
    `namedtuple` instance for the sake of simplicity.

--- species.py
+++ species.py
@@ -1,46 +1,9 @@
 import json
+from collections import namedtuple
 
 SPECIES_FILE = 'species.json'
 
-
-class Species(object):
-
-    '''A species of Pokémon.'''
-
-    __slots__ = ('name', 'num', 'att', 'dfn', 'sta')
-
-    def __init__(self, name, num, att, dfn, sta):
-        self.name = name
-        self.num = num
-        self.att = att
-        self.dfn = dfn
-        self.sta = sta
-
-    def __repr__(self):
-        return '{}({!r}, {!r}, {!r}, {!r}, {!r})'.format(
-            self.__class__.__name__,
-            self.name,
-            self.num,
-            self.att,
-            self.dfn,
-            self.sta)
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        return (self.name == other.name and
-                self.num == other.num and
-                self.att == other.att and
-                self.dfn == other.dfn and
-                self.sta == other.sta)
-
-    def __hash__(self):
-        return (hash(self.name) ^
-                hash(self.num) ^
-                (hash(self.att) << 8) ^
-                (hash(self.dfn) << 16))
-
+Species = namedtuple('Species', ('name', 'num', 'att', 'dfn', 'sta'))
 
 # Populate the records of extant species.
 species = []

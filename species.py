ebcda7973e5464142c5a6779de74a2b9d48aa458
Adam Dinwoodie <adam@dinwoodie.org>
2016-09-02 13:01:21 +0100

    Reference species from a single dict
    Rather than having different dictionaries for referencing different
    species by their name and Pokédex numbers, use the same dictionary for
    both.

--- species.py
+++ species.py
@@ -5,10 +5,10 @@ SPECIES_FILE = 'species.json'
 
 Species = namedtuple('Species', ('name', 'num', 'att', 'dfn', 'sta'))
 
-# Populate the records of extant species.
-species = []
-species_by_number = {}
-species_by_name = {}
+# Populate the records of extant species.  For simplicity, `species` will index
+# by both name and number, so `species['Bulbasaur']` and `species[1]` will both
+# work.
+species = {}
 
 with open(SPECIES_FILE) as species_file:
     for stats in json.load(species_file):
@@ -17,9 +17,8 @@ with open(SPECIES_FILE) as species_file:
                           stats['Att'],
                           stats['Def'],
                           stats['Sta'])
-        species.append(pokemon)
-        species_by_number[pokemon.num] = pokemon
-        species_by_name[pokemon.name] = pokemon
+        species[pokemon.num] = pokemon
+        species[pokemon.name] = pokemon
 
 # Clean up temporary variables.
 del pokemon, species_file

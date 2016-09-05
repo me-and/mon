808f213cf1b73b74aa4f6eb2c92adb15da32ee34
Adam Dinwoodie <adam@dinwoodie.org>
2016-09-05 13:39:28 +0100

    Remove unused import statement

--- iv.py
+++ iv.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 from itertools import count, product
-from math import sqrt
 
 DUST_LEVELS = [200, 400, 600, 800, 1000, 1300, 1600, 1900, 2200, 2500, 3000,
                3500, 4000, 4500, 5000, 6000, 7000, 8000, 9000, 10000]
